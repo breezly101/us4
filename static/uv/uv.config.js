f5224d9ff2f6a2b88a30f44530165ed3f74fdf91
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-10 19:05:33 +0100

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/static/load/',
-    bare:'https://tomp.app/',
+    bare:'https://sw.16jakinyanju.workers.dev/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/static/uv/uv.handler.js',
