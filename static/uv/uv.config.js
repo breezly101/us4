952e7290181db15bcf8a3940adc3ec250f913dbd
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-13 15:52:02 +0100

    Update uv.config.js

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -17,3 +17,116 @@ self.__uv$config = {
 // https://vplaza.org/bare/ - fast
 // https://v2202412246404304352.megasrv.de/bare/
 // https://muddy-rice-ba72.acsessremote.workers.dev/
+
+// Blocked Websites
+
+const blocked = [
+  // Adult / Pornography
+  "xvideos.com",
+  "xnxx.com",
+  "redtube.com",
+  "youporn.com",
+  "xhamster.com",
+  "brazzers.com",
+  "tnaflix.com",
+  "spankwire.com",
+  "tube8.com",
+  "porn.com",
+  "porndig.com",
+  "beeg.com",
+  "sunporno.com",
+  "motherless.com",
+  "fapdu.com",
+  "nuvid.com",
+  "javhd.com",
+  "javlibrary.com",
+  "javhub.net",
+  
+  // Gambling
+  "pokerstars.com",
+  "bet365.com",
+  "williamhill.com",
+  "888casino.com",
+  "betway.com",
+  "ladbrokes.com",
+  "bovada.lv",
+  "draftkings.com",
+  "fanduel.com",
+  "unibet.com",
+  "casino.com",
+  "partycasino.com",
+  "betfred.com",
+  
+  // Violence / Hate / Extremism
+  "stormfront.org",
+  "8chan.moe",
+  "8kun.top",
+  "dailyhate.com",
+  "hatewatch.org",
+  "thepiratebay.org",
+  "gab.com",
+  "infowars.com",
+  
+  // Drugs / Illegal substances
+  "silkroad7rn2puhj.onion",
+  "alphabaymarket.net",
+  "drugstore.com",
+  "drugs.com",
+  "weedmaps.com",
+  "cannabis.com",
+  "420science.com",
+  "bluelight.org",
+  
+  // Risky Social & Chat Platforms
+  "ome.tv",
+  "omegle.com",
+  "chatroulette.com",
+  "chathub.cam",
+  "cam4.com",
+  "bazoocam.org",
+  "camsurf.com",
+  "tinychat.com",
+  "liveme.com",
+  
+  // Other risky sites
+  "4chan.org",
+  "9gag.com",
+  "9anime.to",
+  "mangadex.org",
+  "fakku.net",
+  "fapello.com",
+  "rule34.xxx",
+  "e621.net",
+  "hentaihaven.xxx",
+  "nhentai.net",
+  "sexting.com",
+  
+  // Misc or regional questionable sites
+  "porn555.com",
+  "myfreecams.com",
+  "livejasmin.com",
+  "cams.com",
+  "chaturbate.com",
+  "bonga.com",
+  
+  // Additional gambling & betting
+  "coral.co.uk",
+  "skybet.com",
+  "betfair.com",
+  
+  // Others flagged by some filters
+  "4tube.com",
+  "youjizz.com",
+  "xvideos2.com",
+  "yobt.com"
+];
+
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+
+  if (blocked.some(domain => url.hostname.includes(domain))) {
+    event.respondWith(
+      Response.redirect('/pages/blocked.html', 302)
+    );
+  }
+});
