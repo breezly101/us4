3682f0f8a0566eb2d9161688295e29813b6153c6
breezly_ <122535095+breezly101@users.noreply.github.com>
2025-06-16 02:12:28 +0100

    Update injected-panic.js

--- storage/js/injected-panic.js
+++ storage/js/injected-panic.js
@@ -1,9 +1,11 @@
-const panicKey = localStorage.getItem('panicKey');
-const panicUrl = localStorage.getItem('panicUrl');
-if (panicKey && panicUrl) {
+if (typeof panicKey === 'undefined') {
+  const panicKey = localStorage.getItem('panicKey');
+  const panicUrl = localStorage.getItem('panicUrl');
+  if (panicKey && panicUrl) {
     window.addEventListener('keydown', function (event) {
-        if (event.key === panicKey) {
-            top.location.href = panicUrl;
-        }
+      if (event.key === panicKey) {
+        top.location.href = panicUrl;
+      }
     });
+  }
 }
